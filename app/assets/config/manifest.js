07712ff1998dc1a2c8f82975a2d117a61339e782
Ryan Mulligan <ryan.mulligan@fullharvest.com>
2025-04-24 00:26:19 -0600

    fix(manifest): include TailwindCSS build assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,7 @@
+// Link images and stylesheets
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+// Link compiled TailwindCSS builds
+// Link compiled TailwindCSS builds
+//= link_tree ../builds .css
